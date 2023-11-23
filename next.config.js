e9c0685e037a76962d7b67e0ef73a34085e4af3e
Mohammed Akhlas Hussain <akhlas.hussain@agilealgo.com.sg>
2023-11-23 22:05:52 +0530

    basic-setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com', 'lh3.googleusercontent.com', 'pbxt.replicate.delivery', 'replicate.delivery'],
+      }
+}
 
 module.exports = nextConfig
